9a1732a096e33ad7753afe71eb6aca8fa51e1e81
Crystal Norman <cnorm88@gmail.com>
2020-10-14 00:56:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,3 @@
-# class Triangle
-#   def initialize (a,b,c)
-#     @a = a
-#     @b = b
-#     @c = c
-#   end
-
-#   def kind
-#     all_sides = [@a, @b, @c]
-#     all_sides.sort!
-#     if all_sides.any? {|side| side <=0} 
-#       raise TriangleError
-#     elsif all_sides[0] + all_sides[1] <= all_sides[2]
-#       raise TriangleError
-#     elsif @a == @b && @b == @c
-#       :equilateral
-#     elsif @a != @b && @b != @c && @a != @c               
-#       :scalene
-#     else
-#       :isosceles
-#     end
-#   end
-
-  
-# end
-
-
 class Triangle
   attr_accessor :sides
   
